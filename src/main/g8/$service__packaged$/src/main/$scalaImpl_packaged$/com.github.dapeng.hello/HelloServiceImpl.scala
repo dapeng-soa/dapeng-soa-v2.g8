271c54802ad3e2598de4804271885e4abb44375c
jackliang18588733858 <18588733858@163.com>
2018-11-19 01:51:38 +0800

    add default service implementation

---
+++ src/main/g8/$service__packaged$/src/main/$scalaImpl_packaged$/com.github.dapeng.hello/HelloServiceImpl.scala
@@ -0,0 +1,19 @@
+package com.github.dapeng.hello
+
+import com.github.dapeng.hello.scala.domain.Hello
+import com.github.dapeng.hello.scala.service.HelloService
+
+class HelloServiceImpl extends HelloService{
+  /**
+    *
+    **
+  say hello
+    *
+    **/
+  override def sayHello(name: String): String = s"has received $name"
+
+  /**
+    *
+    **/
+  override def sayHello2(hello: Hello): String = s"has received: $hello"
+}
