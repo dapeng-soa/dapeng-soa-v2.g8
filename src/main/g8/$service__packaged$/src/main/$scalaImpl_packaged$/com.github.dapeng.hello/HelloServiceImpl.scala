400648f1564be1b01d9f4375ca5cbfa801e6867b
jackliang18588733858 <18588733858@163.com>
2018-11-19 01:55:18 +0800

    enhance impl response

--- src/main/g8/$service__packaged$/src/main/$scalaImpl_packaged$/com.github.dapeng.hello/HelloServiceImpl.scala
+++ src/main/g8/$service__packaged$/src/main/$scalaImpl_packaged$/com.github.dapeng.hello/HelloServiceImpl.scala
@@ -10,10 +10,10 @@ class HelloServiceImpl extends HelloService{
   say hello
     *
     **/
-  override def sayHello(name: String): String = s"has received $name"
+  override def sayHello(name: String): String = s"has received msg:  $name"
 
   /**
     *
     **/
-  override def sayHello2(hello: Hello): String = s"has received: $hello"
+  override def sayHello2(hello: Hello): String = s"has received msg: $hello"
 }
